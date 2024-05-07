63ec91ffa387cc205237b8daf493d1d9548dbbbe
Tshmytzn <128598268+Tshmytzn@users.noreply.github.com>
2024-05-07 16:00:51 +0800

    update

--- routes/web.php
+++ routes/web.php
@@ -17,8 +17,3 @@ use Illuminate\Support\Facades\Route;
 Route::get('/Admin', function () { return view('admin.index'); })->name('AdminDashboard');
 // Route::get('/', function () {return view('index');})->name('index');
 Route::get('/blank', function () { return view('admin.blank'); })->name('Blank');
-=======
-Route::get('/', function () {
-    return view('welcome');
-});
-
