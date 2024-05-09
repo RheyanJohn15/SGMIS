7bfee75387fbb936ee556944c155b7cb951a824c
JpUbas <jpubas@gmail.com>
2024-05-09 11:33:39 +0800

    WEB UPDATE

--- routes/web.php
+++ routes/web.php
@@ -13,7 +13,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-
 Route::get('/Admin', function () { return view('admin.index'); })->name('AdminDashboard');
 Route::get('/Accounts', function () { return view('admin.accounts'); })->name('Accounts');
 Route::get('/Events', function () { return view('admin.events'); })->name('Events');
